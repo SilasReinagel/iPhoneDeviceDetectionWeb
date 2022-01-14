a37c45604b01f71d08bad1231354d38cfc688bbd
Silas Reinagel <silas.reinagel@carvana.com>
2022-01-14 13:24:40 -0700

    Trying to improve dectection of later iPhone models.

--- iphoneDeviceDetect.js
+++ iphoneDeviceDetect.js
@@ -1,30 +1,39 @@
-function iPhoneDeviceModel() {
+const detectionVersion = '0.02';
+
+let detectionRenderer;
+
+const getDetectedGpu = () => {
+    if (!detectionRenderer)
+        return "None";
+    return detectionRenderer;
+}
+
+const iPhoneDeviceModel = () => {
   const canvas = document.createElement("canvas");
-  let renderer;
   if (canvas) {
-      const context = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-      if (context) {
-          const info = context.getExtension("WEBGL_debug_renderer_info");
-          if (info) {
-              renderer = context.getParameter(info.UNMASKED_RENDERER_WEBGL);
+      const gl = canvas.getContext("webgl");
+      if (gl) {
+          const debugInfo = gl.getExtension("WEBGL_debug_renderer_info");
+          if (debugInfo) {
+              detectionRenderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
           }
       }
   }
 
   // iPhone 13 Series
-  if (renderer == "Apple A15 GPU")
+  if (detectionRenderer == "Apple A15 GPU")
     return "iPhone 13 Series";
   
   // iPhone 12 Series
-  if (renderer == "Apple A14 GPU")
+  if (detectionRenderer == "Apple A14 GPU")
     return "iPhone 12 Series";
 
   // iPhone 11 Series
-  if (renderer == "Apple A13 GPU")
+  if (detectionRenderer == "Apple A13 GPU")
     return "iPhone 11 Series";
 
   // iPhone XS Series (including XR)
-  if (renderer == "Apple A12 GPU")
+  if (detectionRenderer == "Apple A12 GPU")
     return "iPhone XS Series";
 
   // Below from StackOverflow: https://stackoverflow.com/a/49774317
@@ -35,7 +44,7 @@ function iPhoneDeviceModel() {
 
   // iPhone 6+/6s+/7+ and 8+
   } else if ((window.screen.height / window.screen.width == 736 / 414) && (window.devicePixelRatio == 3)) {
-      switch (renderer) {
+      switch (detectionRenderer) {
           default:
               return "iPhone 6 Plus, 6s Plus, 7 Plus or 8 Plus";
           case "Apple A8 GPU":
@@ -50,7 +59,7 @@ function iPhoneDeviceModel() {
 
   // iPhone 6+/6s+/7+ and 8+ in zoom mode
   } else if ((window.screen.height / window.screen.width == 667 / 375) && (window.devicePixelRatio == 3)) {
-      switch(renderer) {
+      switch(detectionRenderer) {
           default:
               return "iPhone 6 Plus, 6s Plus, 7 Plus or 8 Plus (display zoom)";
           case "Apple A8 GPU":
@@ -65,7 +74,7 @@ function iPhoneDeviceModel() {
 
   // iPhone 6/6s/7 and 8
   } else if ((window.screen.height / window.screen.width == 667 / 375) && (window.devicePixelRatio == 2)) {
-      switch(renderer) {
+      switch(detectionRenderer) {
           default:
               return "iPhone 6, 6s, 7 or 8";
           case "Apple A8 GPU":
@@ -80,7 +89,7 @@ function iPhoneDeviceModel() {
 
   // iPhone 5/5C/5s/SE or 6/6s/7 and 8 in zoom mode
   } else if ((window.screen.height / window.screen.width == 1.775) && (window.devicePixelRatio == 2)) {
-      switch(renderer) {
+      switch(detectionRenderer) {
           default:
               return "iPhone 5, 5C, 5S, SE or 6, 6s, 7 and 8 (display zoom)";
           case "PowerVR SGX 543":
@@ -99,7 +108,7 @@ function iPhoneDeviceModel() {
 
   // iPhone 4/4s
   } else if ((window.screen.height / window.screen.width == 1.5) && (window.devicePixelRatio == 2)) {
-      switch(renderer) {
+      switch(detectionRenderer) {
           default:
               return "iPhone 4 or 4s";
           case "PowerVR SGX 535":
@@ -110,7 +119,7 @@ function iPhoneDeviceModel() {
 
   // iPhone 1/3G/3GS
   } else if ((window.screen.height / window.screen.width == 1.5) && (window.devicePixelRatio == 1)) {
-      switch(renderer) {
+      switch(detectionRenderer) {
           default:
               return "iPhone 1, 3G or 3GS";
           case "ALP0298C05":
@@ -118,7 +127,14 @@ function iPhoneDeviceModel() {
           case "S5L8900":
               return "iPhone 1, 3G";
       }
+  } else if (isIphone()) {
+    return "Unindentified iPhone";
   } else {
-      return "Probably not an iPhone";
+    return "Probably not an iPhone";
   }
+}
+
+// From StackOverflow: https://stackoverflow.com/a/9039885/4009517
+const isIphone = () => {
+    return ['iPhone Simulator','iPhone'].includes(navigator.platform);
 }
